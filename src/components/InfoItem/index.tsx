aac92437dc95548dd987aadc4d71316fcf4efee8
khristopher <khristopheroficial@gmail.com>
2023-05-15 13:30:32 -0300

    Jogo da Memoria

---
+++ src/components/InfoItem/index.tsx
@@ -0,0 +1,16 @@
+import * as C from './styles';
+
+type Props = {
+    label: string;
+    value: string;
+}
+
+export const InfoItem = ({ label, value}: Props) => {
+
+    return(
+        <C.Container>
+            <C.label>{label}</C.label>
+            <C.Value>{value}</C.Value>
+        </C.Container>
+    );
+}
